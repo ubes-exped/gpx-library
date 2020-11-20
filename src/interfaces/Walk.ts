99637cb93028143c5c3ba4095129e5d2e972c051
Charlie Harding <charlie_harding@icloud.com>
2020-11-20 21:56:00 +0100

    Add walk class

--- src/interfaces/Walk.ts
+++ src/interfaces/Walk.ts
@@ -1,7 +1,36 @@
-export default interface Walk {
-  id: number;
-  name: string;
-  distance: number;
-  elevation: number;
-  route: string;
+export interface WalkSegment {
+heights: number[];
+length: number;
+polyline: string;
+}
+
+export interface RawWalk {
+  description?: string;
+  name?: string;
+  filename: string;
+  segments: WalkSegment[];
+}
+
+export default class Walk {
+  readonly description?: string;
+
+  readonly name?: string;
+
+  readonly filename: string;
+
+  readonly segments: WalkSegment[];
+
+  constructor(walk: RawWalk) {
+    this.description = walk.description;
+    this.name = walk.name;
+    this.filename = walk.filename;
+    this.segments = walk.segments;
+  }
+
+  #distance?: number;
+
+  get distance() {
+    this.#distance ?? (this.#distance = this.segments.reduce((acc, segment) => acc + segment.length, 0));
+    return this.#distance;
+  }
 }
