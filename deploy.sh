37e69bf5c8aee57e7d319c826a98a2f71865a099
Charlie Harding <charlie_harding@icloud.com>
2020-11-21 22:07:58 +0100

    Deploy with tgz

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
-(echo "Clearing the old version"; find ./dist/ -mindepth 1 -delete) &&
-(echo "Unzipping"; unzip ./dist > /dev/null) &&
 
-(echo "Setting permissions"; find ./dist/ -type d -exec chmod 755 {} \; && find ./dist/ -type f -exec chmod 644 {} \;)
+(echo "Clearing the old version"; mkdir -p dist && find ./dist/ -mindepth 1 -delete) &&
+(echo "Unzipping"; tar -xzf dist.tar.gz) &&
+
+(echo "Setting permissions"; chmod 755 dist && find ./dist/ -type d -exec chmod 755 {} \; && find ./dist/ -type f -exec chmod 644 {} \;)
