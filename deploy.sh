ecf8da9858eab5dbfdfbfb2a7e54bfae143ccb18
Charlie Harding <charlie_harding@icloud.com>
2020-11-21 04:16:08 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+(echo "Clearing the old version"; find ./dist/ -mindepth 1 -delete) &&
+(echo "Unzipping"; unzip ./dist > /dev/null) &&
+
+(echo "Setting permissions"; find ./dist/ -type d -exec chmod 755 {} \; && find ./dist/ -type f -exec chmod 644 {} \;)
