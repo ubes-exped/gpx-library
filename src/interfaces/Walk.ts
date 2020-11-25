1bae6c0930872589e12da5704b4de0f3d24fe674
Charlie Harding <charlie_harding@icloud.com>
2020-11-26 00:38:46 +0100

    Update to match the new data model in 13b8d4af

--- src/interfaces/Walk.ts
+++ src/interfaces/Walk.ts
@@ -13,16 +13,10 @@ export interface RawWalk {
   author?: string;
   tags?: string[];
 
-  heights: number[];
+  ele: string;
+  ascent: number;
   length: number;
   polyline: string;
-}
-
-function getAscent(heights: number[]): number {
-  return heights.reduce(
-    ({ prev, ascent }, next) => ({ prev: next, ascent: ascent + Math.max(0, next - prev) }),
-    { prev: +Infinity, ascent: 0 },
-  ).ascent;
 }
 
 export interface PointOnLine {
@@ -44,10 +38,12 @@ export default class Walk {
 
   readonly tags?: string[];
 
-  readonly heights: number[];
+  readonly heights: string;
 
   /** The length of the route in metres */
   readonly length: number;
+
+  readonly ascent: number;
 
   readonly polyline: string;
 
@@ -63,7 +59,8 @@ export default class Walk {
     this.description = walk.description;
     this.author = walk.author;
     this.tags = walk.tags;
-    this.heights = walk.heights;
+    this.heights = walk.ele;
+    this.ascent = walk.ascent;
     this.length = walk.length;
     this.polyline = walk.polyline;
 
@@ -76,14 +73,6 @@ export default class Walk {
     return this.length / 1000;
   }
 
-  #ascent?: number;
-
-  /** Total ascent of the walk in metres. This is calculated once. */
-  get ascent(): number {
-    this.#ascent ??= getAscent(this.heights);
-    return this.#ascent;
-  }
-
   get href(): string {
     return new URL(`/${this.filename}`, routesFile).href;
   }
@@ -91,10 +80,17 @@ export default class Walk {
   get elevationGraph() {
     const width = 500;
     let height = 100;
-    const step = width / (this.heights.length - 1);
-    const { min: minElevation, max: maxElevation } = this.heights.reduce(({ min, max }, next) => ({
-      min: Math.min(min, next), max: Math.max(max, next),
-    }), { min: +Infinity, max: -Infinity });
+
+    // distance, height in metres. They are served from the server in hundreds of kilometres
+    // for efficiency (so resolution is exactly 1m).
+    const points = polyline.decode(this.heights).map((point) => point.map((c) => c * 1e5));
+
+    const { min: minElevation, max: maxElevation } = points.reduce(
+      ({ min, max }, [, elevation]) => ({
+        min: Math.min(min, elevation), max: Math.max(max, elevation),
+      }),
+      { min: +Infinity, max: -Infinity },
+    );
     const elevationRange = maxElevation - minElevation;
 
     // Ensure the graph never exaggerates too much
@@ -104,13 +100,14 @@ export default class Walk {
     }
 
     const yValue = (elevation: number) => ((maxElevation - elevation) / elevationRange) * height;
-    const points = this.heights
-      .map((elevation, index) => [index * step, yValue(elevation)].join(" "))
+    const scale = width / this.length;
+    const pathString = points
+      .map(([distance, elevation]) => [distance * scale, yValue(elevation)].join(" "))
       .join("L");
 
     return `
       <svg viewBox="0 -2 ${width} ${height + 4}" xmlns="http://www.w3.org/2000/svg">
-        <path d="M${points}"
+        <path d="M${pathString}"
           stroke="var(--color)"
           stroke-width="4"
           stroke-linecap="square"
