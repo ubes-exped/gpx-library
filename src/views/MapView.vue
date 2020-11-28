4de6917ee1ff0a97b09ca161870deb655b61e4fb
Charlie Harding <charlie_harding@icloud.com>
2020-11-28 12:52:54 +0100

    Load the map in a separate chunk

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -23,13 +23,16 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop, Watch } from "vue-property-decorator";
-import Map from "@/components/Map.vue";
 import Sidebar from "@/components/Sidebar.vue";
 import Help from "@/components/Help.vue";
 import Walk, { PointOnLine } from "@/interfaces/Walk";
 
 @Component({
-  components: { Map, Sidebar, Help }
+  components: {
+    Map: () => import(/* webpackChunkName: "map" */ "@/components/Map.vue"),
+    Sidebar,
+    Help,
+  },
 })
 export default class MapView extends Vue {
   @Prop() walks!: Walk[];
@@ -49,13 +52,10 @@ export default class MapView extends Vue {
   get filteredWalks(): Walk[] {
     if (!this.tagFilter || !this.useTags) return this.walks;
 
-    let foundSelected = false;
-    const filtered = this.walks.filter(walk =>
-      walk.tags?.includes(this.tagFilter)
-    );
+    const filtered = this.walks.filter((walk) => walk.tags?.includes(this.tagFilter));
     if (
-      this.selected &&
-      !filtered.find(walk => walk.index === this.selected?.index)
+      this.selected
+      && !filtered.find((walk) => walk.index === this.selected?.index)
     ) {
       this.selected = null;
     }
@@ -67,7 +67,7 @@ export default class MapView extends Vue {
    */
   get allTags() {
     return Array.from(
-      new Set(this.walks.flatMap(walk => walk.tags ?? []))
+      new Set(this.walks.flatMap((walk) => walk.tags ?? [])),
     ).sort((a, b) => a.localeCompare(b));
   }
 
@@ -82,8 +82,7 @@ export default class MapView extends Vue {
   @Watch("selectedHash") @Watch("walks", { immediate: true }) onWalks() {
     const browserSelected = this.selectedHash;
     if (browserSelected) {
-      this.selected =
-        this.walks.find(walk => walk.id === browserSelected) ?? null;
+      this.selected = this.walks.find((walk) => walk.id === browserSelected) ?? null;
     } else {
       this.selected = null;
     }
