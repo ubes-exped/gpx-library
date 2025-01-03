a0c8909a7b263b4a4182fb9394326a44ef399ff6
Charlie Harding <charlie_harding@icloud.com>
2025-01-03 19:39:05 +0000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ REPO_URL="git@github.com:ubes-exped/routes.ubes.co.uk.git"
 (echo "Unzipping"; tar -xzf dist.tar.gz) &&
 (echo "Clearing cache"; rm walks.json 2> /dev/null; true) &&
 (echo "Setting permissions"; chmod 755 dist && find ./dist/ -type d -exec chmod 755 {} \; && find ./dist/ -type f -exec chmod 644 {} \;) &&
-(echo "Cloning repo"; cd dist; git clone "$REPO_URL")
+(echo "Cloning repo"; cd dist && GIT_SSH_COMMAND="ssh -i ../github_deploy" git clone "$REPO_URL")
